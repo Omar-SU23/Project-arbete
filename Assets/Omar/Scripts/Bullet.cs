81630f40275284b3fceedc77e1e207b35fd148c2
Omar-SU23 <157362246+Omar-SU23@users.noreply.github.com>
2024-02-05 11:10:17 +0100

    Shooting & Bullet

---
+++ Assets/Omar/Scripts/Bullet.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public int dmg = 10;
+    public int speed = 10;
+    public int knockback = 1;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
