50385577d22f4c91dc5996edc5fb57e89543b24b
Omar-SU23 <157362246+Omar-SU23@users.noreply.github.com>
2024-02-05 13:41:12 +0100

    Shooting & Bullet

--- Assets/Omar/Scripts/Bullet.cs
+++ Assets/Omar/Scripts/Bullet.cs
@@ -5,17 +5,25 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     public int dmg = 10;
-    public int speed = 10;
-    public int knockback = 1;
+    public float speed = 10f;
+    public float knockback = 1f;
+    public float lifeTime = 5f;
 
     void Start()
     {
-        
+        GetComponent<Rigidbody2D>().AddForce(transform.right * speed, ForceMode2D.Impulse);
+        Destroy(gameObject, lifeTime);
     }
 
     
     void Update()
     {
         
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+            return;
+
     }
 }
--- Assets/Omar/Scripts/Shooting.cs
+++ Assets/Omar/Scripts/Shooting.cs
@@ -4,16 +4,36 @@ using UnityEngine;
 
 public class Shooting : MonoBehaviour
 {
-    public int ammo = 0;
+    public PlayerController player;
+    public GameObject bulletPrefab;
+    public Transform spawnPoint;
+
+    public int ammo = 1;
+    public bool autoFire = true;
+    public float shootingRate = 0.24f;
+    private float shootingTimer = 0f;
 
     void Start()
     {
-        
+
     }
 
-    
+
     void Update()
     {
-        
+        if (Input.GetButtonDown("Fire"))
+        {
+            if (ammo >= 0)
+            {
+                float angle = Mathf.Atan2(player.lastDir.y, player.lastDir.x) * Mathf.Rad2Deg;
+
+                GameObject bullet = Instantiate(bulletPrefab, spawnPoint.position, Quaternion.Euler(0f, 0f, angle));
+                ammo--;
+            }
+        }
+    }
+    public void AddAmmo(int amount)
+    {
+        ammo += amount;
     }
 }
