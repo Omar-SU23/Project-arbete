eb4a7fe7e399d76cc13cb8532cad17a08b8ba069
Jonathan-Mellberg <158470120+Jonathan-Mellberg@users.noreply.github.com>
2024-02-05 11:08:59 +0100

    Camera och player move

---
+++ Assets/Jonathan/Jonathan_move.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jonathan_move : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    //private SpriteRenderer spriteRenderer;
+    private Animator animator;
+
+    public float speed = 5f;
+    private Vector2 moveDir;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+        //spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+
+    void Update()
+    {
+        moveDir = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        if (moveDir.magnitude > 1f)
+        {
+            moveDir.Normalize();
+        }
+
+        // Set animation parameters
+        if (moveDir.sqrMagnitude > 0.01f)
+        {
+            animator.SetFloat("xMove", moveDir.x);
+            animator.SetFloat("yMove", moveDir.y);
+
+
+            //flip player
+            //spriteRenderer.flipX = moveDir.x > 0f ? true : false;
+        }
+        animator.SetBool("moving", moveDir.sqrMagnitude > 0.01f ? true : false);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveDir * speed * Time.fixedDeltaTime);
+    }
+}
