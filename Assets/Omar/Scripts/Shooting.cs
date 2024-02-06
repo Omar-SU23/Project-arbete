fde98af2587c7150bdd1a8f7e9f5c5d8ea17b665
Omar-SU23 <157362246+Omar-SU23@users.noreply.github.com>
2024-02-06 13:40:21 +0100

    Gun rotation

--- Assets/Omar/Scripts/Shooting.cs
+++ Assets/Omar/Scripts/Shooting.cs
@@ -8,33 +8,39 @@ public class Shooting : MonoBehaviour
     public GameObject bulletPrefab;
     public Transform spawnPoint;
     private SpriteRenderer spriteRenderer;
+    private Transform mTransform;
 
+    public float angle;
     public int ammo = 1;
     public bool autoFire = true;
     public float shootingRate = 0.24f;
     private float shootingTimer = 0f;
 
-    void Start()
+    private void Start()
     {
-
+        mTransform = this.transform;
     }
 
+    private void LAMouse()
+    {
+        Vector2 direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - mTransform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle - 90, Vector3.forward);
+        mTransform.rotation = rotation;
 
+    }
     void Update()
     {
         if (Input.GetButtonDown("Fire"))
         {
+            
             if (ammo >= 0)
-            {
-                float angle = Mathf.Atan2(player.lastDir.y, player.lastDir.x) * Mathf.Rad2Deg;
-
+            {                
                 GameObject bullet = Instantiate(bulletPrefab, spawnPoint.position, Quaternion.Euler(0f, 0f, angle));
                 ammo--;
             }
         }
-        float rotation = Input.GetAxis("mouse X" + "mouse Y");
-        transform.Rotate(0f, 0f, rotation);
-
+        LAMouse();
         
     }
     public void AddAmmo(int amount)
